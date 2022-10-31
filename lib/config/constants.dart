a25cc8172134688886c2feef85e4c6606e960e72
mohamedkh242 <mohamedkholif710@gmail.com>
2022-10-31 13:16:10 +0200

    refresh token

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,4 +1,4 @@
 const String appId = '7e727bdb6a114fa8a26a499a68845489';
 const String token =
-    '007eJxTYEgNmqL2/Wbq8o83E1hOcHSvmX5522od56sxP+/urb6dqdypwGCeam5knpSSZJZoaGiSlmiRaGSWaGJpmWhmYWFiamJh+U8nLrkhkJFhR9ZvFkYGCATxORkS0/OLEpMTc3IYGADBJCQE';
+    '007eJxTYOCM+af6WCt17bRgqTm/e07KeJcxX+NvqVb7eftK9FSmJ54KDOap5kbmSSlJZomGhiZpiRaJRmaJJpaWiWYWFiamJhaWN9bGJzcEMjKsL7nOwsgAgSA+J0Nien5RYnJiTg4DAwDgHSGZ';
 const String channelName = "agoracall";
